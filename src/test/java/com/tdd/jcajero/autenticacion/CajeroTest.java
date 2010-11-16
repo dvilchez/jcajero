d9b3d8cddd10f69e924928f8be550bd5c5b7b630
Alejandro Perez Garcia <alejandropg@autentia.com>
2010-11-16 16:03:03 +0100

    Despues del merge

--- src/test/java/com/tdd/jcajero/autenticacion/CajeroTest.java
+++ src/test/java/com/tdd/jcajero/autenticacion/CajeroTest.java
@@ -4,13 +4,15 @@ import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
 
+import com.tdd.jcajero.operaciones.saldo.Saldo;
 
 public abstract class CajeroTest {
 
 	protected static final Pin ALICIA_PIN = new Pin(new Digito(7), new Digito(6), new Digito(4), new Digito(3));
+
 	protected static final Pin ERNESTO_PIN = new Pin(new Digito(5), new Digito(9), new Digito(5), new Digito(4));
-	
-	
+
+	protected static final Saldo SALDO_DE_ALICIA = new Saldo(2000);
 
 	@Test
 	public void aceptarTarjetaCorrecta() throws Exception {
